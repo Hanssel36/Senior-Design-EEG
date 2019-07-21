6292168a4f66d4bd523fe669120d0171135d2176
Hanssel36 <hanssel36@gmail.com>
2019-07-21 16:03:28 -0400

    Fibit Graph

--- Heart_rate_plot.py
+++ Heart_rate_plot.py
@@ -7,9 +7,14 @@ import mplcursors
 def Event_line(**kwargs):
     for key,val in kwargs.items(): # key is the event name and val is the corresponding time 
         for j in HR_data.time: 
-            if val == j: # checks to see if the picked event time is in the readings
-                plt.axvline(x=val, linestyle='dashed', alpha=0.5)
-                plt.text(x=val, y=min(HR_data.heartrate), s=key, alpha=0.7, color='#334f8d')
+            if j == val: # checks to see if the picked event time is in the readings
+                plt.axvline(x=j, linestyle='dashed', alpha=0.5)
+                plt.text(x=j, y=min(HR_data.heartrate), s=key, alpha=0.7, color='#334f8d')
+                break
+            elif j > val: # If the selected time is not in the reads it will pick the closes thing to the rightmost. Ex 10:37:50 (not in data) will estimate to 10:37:52 (in data)
+                plt.axvline(x=j, linestyle='dashed', alpha=0.5)
+                plt.text(x=j, y=min(HR_data.heartrate), s=key, alpha=0.7, color='#334f8d')
+                break
 
 HR_data = pd.read_csv('heartrate_data.csv')
 #Reads CSV file and can turn HR_data into a list 
@@ -28,5 +33,5 @@ plt.title('HRData_Subway')
 plt.xlabel('Time')
 plt.ylabel('HeartRate(BPM)')
 plt.xticks(np.arange(len(HR_data.time), step=10))
-Event_line(event1 = '10:37:52', event2 = '10:44:54')
+Event_line(event1 = '09:37:50', event2 = '10:45:00')
 plt.show()
